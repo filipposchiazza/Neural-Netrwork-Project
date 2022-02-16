3f9aaaeebd371efe93e8158930bc9750c15c5433
Filippo Schiazza <filipposchiazza97@gmail.com>
2022-02-16 11:37:42 +0100

    Create the skeleton of the class Ann

--- ann.py
+++ ann.py
@@ -1,2 +1,26 @@
 "Diamo inizio al gioco...vediamo se mi ricordo ancora come si fa"
 
+
+class Ann:
+    
+    def __init__(self):
+        pass
+    
+    def _forward_prop(self):
+        pass
+    
+    def _backward_prop(self):
+        pass
+    
+    def _gradient_descendent(self):
+        pass
+    
+    def train(self):
+        pass
+    
+    def predict(self):
+        pass
+    
+    
+    
+        
