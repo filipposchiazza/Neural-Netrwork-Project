4ffd9d757f02407ff23e79e028e3ab291e611f0a
Filippo Schiazza <filipposchiazza97@gmail.com>
2022-02-17 12:47:41 +0100

    Create the file containing the activation functions and their derivative

---
+++ activation_functions.py
@@ -0,0 +1,36 @@
+"Different type of activation function and their derivatives to be used in the Artificial Neural Network"
+
+import numpy as np
+
+
+def sigmoid(x):
+    "Definition of the sigmoid function"
+    return 1 / (1 + np.exp(-x))
+
+def deriv_sigmoid(x):
+    "Definition of the derivative of the sigmoid function"
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def relu(x):
+    "Definition of the Rectified Linear Unit function"
+    return max(0, x)
+
+def deriv_relu(x):
+    "Definition of the derivative of the Rectified Linear Unit function"
+    if x<0:
+        return 0
+    else:
+        return 1
+   
+    
+def tanh(x):
+    "Definition of the hyperbolic function tanh"
+    num = 1 - np.exp(-2 * x)
+    den = 1 + np.exp(-2 * x)
+    return num/den
+
+def deriv_tanh(x):
+    "Definition of the derivative of the hyperbolic function tanh"
+    return 1 - tanh(x)**2
+
