5aa428fd0f487523458df61d4a143e40b2a45ebb
Filippo Schiazza <filipposchiazza97@gmail.com>
2022-02-23 11:28:27 +0100

    Added tests on the dimensions of lists used to store derivatives and biases

--- testing.py
+++ testing.py
@@ -19,7 +19,7 @@ num_neuron = [i for i in range(max_num_neurons + 1)]
        hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
        out = st.integers(min_value=1, max_value=50))
 def test_num_layers(inp, hidd, out):
-    #Test if the number of layers is the correct one, according to the inputs given to build the ann'
+    #Test if the number of layers is the correct one, according to the inputs given to build the ann
     network = ann.Ann(inp, hidd, out)
     assert network.layers.size == len([network.num_inputs]) + network.num_hidden.size + len([network.num_outputs])
  
@@ -28,20 +28,46 @@ def test_num_layers(inp, hidd, out):
        hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
        out = st.integers(min_value=1, max_value=50))   
 def test_num_weights(inp, hidd, out):
-    # Test if the total number of weights of the neural network is the correct one, according to the inputs given to build it'
+    # Test if the total number of weights of the neural network is the correct one, according to the inputs given to build it
     network = ann.Ann(inp, hidd, out)
-    
     counter_first = 0
-    for i in range(network.layers.size-1):
-        counter_first += (network.layers[i] + 1) * (network.layers[i+1])
-    
+    for i in range(len(network.layers)-1):
+        counter_first += (network.layers[i]) * (network.layers[i+1])
     counter_second = 0
     for i in range(len(network.weights)):
         for j in range(len(network.weights[i])):
             counter_second += len(network.weights[i][j])
-          
     assert counter_first == counter_second
-    
+
+
+@given(inp = st.integers(min_value=1, max_value=1e5),
+       hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
+       out = st.integers(min_value=1, max_value=50)) 
+def test_num_biases(inp, hidd, out):
+    # Test if the total number of biases is equal to (total number of neurons - number of neurons in the input layer)
+    network = ann.Ann(inp, hidd, out)
+    counter_first = 0
+    for b in network.biases:
+        counter_first += b.size
+    counter_second = 0
+    for i in range(1, len(network.layers)):
+        counter_second += network.layers[i]
+    assert counter_first == counter_second
+
+
+@given(inp = st.integers(min_value=1, max_value=1e5),
+       hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
+       out = st.integers(min_value=1, max_value=50))
+def test_num_linear_comb(inp, hidd, out):
+    # Test if the total number of linear combinations is equal to (total number of neurons - number of neurons in the input layer)
+    network = ann.Ann(inp, hidd, out)
+    counter_first = 0
+    for lin in network.linear_comb:
+        counter_first += lin.size
+    counter_second = 0
+    for i in range(1, len(network.layers)):
+        counter_second += network.layers[i]
+    assert counter_first == counter_second
     
 @given(inp = st.integers(min_value=1, max_value=1e5),
        hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
@@ -50,16 +76,27 @@ def test_num_activations(inp, hidd, out):
     # Test if the numbers of activations stored for each layer is the correct one (corresponding to the number of neurons)
     network = ann.Ann(inp, hidd, out)
     for i in range(len(network.layers)):
-        assert len(network.activations[i]) == network.layers[i]
+        assert len(network.activations[i]) == network.layers[i] 
+
         
 @given(inp = st.integers(min_value=1, max_value=1e5),
        hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
        out = st.integers(min_value=1, max_value=50)) 
-def test_num_derivatives(inp, hidd, out):
-    # Test if the number of derivatives stored is the same of the number of weights (as it should be)
+def test_num_weights_derivatives(inp, hidd, out):
+    # Test if the number of weights'derivatives stored is the same of the number of weights (as it should be)
     network = ann.Ann(inp, hidd, out)
     for i in range(len(network.weights)):
-        assert network.weights[i].size == network.derivatives[i].size
+        assert network.weights[i].size == network.weights_deriv[i].size
+
+
+@given(inp = st.integers(min_value=1, max_value=1e5),
+       hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
+       out = st.integers(min_value=1, max_value=50)) 
+def test_num_biases_derivatives(inp, hidd, out):
+    # Test if the number of biases'derivatives stored is the same of the number of weights (as it should be)
+    network = ann.Ann(inp, hidd, out)
+    for i in range(len(network.biases)):
+        assert network.biases[i].size == network.biases_deriv[i].size
 
 ##########################################################################################################################
  
