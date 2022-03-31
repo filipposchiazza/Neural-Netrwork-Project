ce29c0bd9378c8779ea836fe83b8750e1caee5cc
Filippo Schiazza <filipposchiazza97@gmail.com>
2022-03-31 17:12:52 +0200

    Test saving and loading methods

--- testing.py
+++ testing.py
@@ -4,6 +4,8 @@ import numpy as np
 from hypothesis import given
 from hypothesis.strategies import data
 import hypothesis.strategies as st
+import pickle
+import os
 
 import ann
 import activation_functions as act
@@ -15,13 +17,13 @@ num_neuron = [i for i in range(1, max_num_neurons + 1)]
 """
 ##################################################################################################################################
 
-# Test the costruction of the neural network (the function __init__)
+#Test the costruction of the neural network (the function __init__)
 
 @given(inp = st.integers(min_value=1, max_value=1e5),
        hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
        out = st.integers(min_value=1, max_value=50))
 def test_num_layers(inp, hidd, out):
-    #Test if the number of layers is the correct one, according to the inputs given to build the ann
+    # Test if the number of layers is the correct one, according to the inputs given to build the ann
     network = ann.Ann(inp, hidd, out)
     assert network.layers.size == len([network.num_inputs]) + network.num_hidden.size + len([network.num_outputs])
  
@@ -171,6 +173,7 @@ def test_weights_structure_after_gradient (inp, hidd, out, learning_rate):
         for j in range(len(network.weights[i])):
             assert network.weights[i][j].size == previous_weights[i][j].size
             
+            
 @given(inp = st.integers(min_value=1, max_value=1e5),
        hidd = st.lists(st.sampled_from(num_neuron), min_size=0, max_size=10),
        out = st.integers(min_value=1, max_value=50),
@@ -201,18 +204,90 @@ def test_biases_structure_after_gradient (inp, hidd, out, learning_rate):
 
 ########################################################################################################################## 
 
-# Test the evaluate method
+# Test the evaluate classification method
 
-##########################################################################################################################  
-
-
+##########################################################################################################################
 """
+# Test save parameters method
+@given(inp = st.integers(min_value=1, max_value=1e5),
+       hidd = st.lists(st.sampled_from(num_neuron), min_size=1, max_size=10),
+       out = st.integers(min_value=1, max_value=50),
+       file_name = st.text(alphabet = st.characters(whitelist_categories = ('L', 'N'))))
+def test_save_correctly(inp, hidd, out, file_name):
+    "Test that all the parameters of the neural network are saved correctly"
+    ann_test = ann.Ann(inp, hidd, out)
+    ann_test.set_activation_function(act.sigmoid)
+    ann_test.set_loss_function(lf.binary_cross_entropy)
+    ann_test.save_parameters(file_name)
+    parameters = pickle.load(open("./" + file_name + ".pkl", 'rb'))
+    os.remove("./" + file_name + ".pkl")
+    assert parameters[0] == ann_test.num_inputs
+    assert np.all(parameters[1] == ann_test.num_hidden)
+    assert parameters[2] == ann_test.num_outputs
+    for i in range(len(parameters[3])):
+        assert np.all(parameters[3][i] == ann_test.get_biases()[i])
+    for i in range(len(parameters[4])):
+        assert np.all(parameters[4][i] == ann_test.get_weights()[i])
+    assert parameters[5] == ann_test.get_activation_function()
+    assert parameters[6] == ann_test.get_loss_function()
 
+##########################################################################################################################
+
+# Test load parameters method
+@given(inp = st.integers(min_value=1, max_value=1e5),
+       hidd = st.lists(st.sampled_from(num_neuron), min_size=1, max_size=10),
+       out = st.integers(min_value=1, max_value=50),
+       file_name = st.text(alphabet = st.characters(whitelist_categories = ('L', 'N'))))
+def test_loading_parameters(inp, hidd, out, file_name):
+    "Test that all the parameters of the neural network are loaded correctly"
+    ann_test = ann.Ann(inp, hidd, out)
+    ann_test.set_activation_function(act.sigmoid)
+    ann_test.set_loss_function(lf.binary_cross_entropy)
+    ann_test.save_parameters(file_name)
+    biases, weights, num_inputs, num_hidd, num_outputs, activation_function, loss_function = ann.Ann.load_parameters("./" + file_name + ".pkl")
+    os.remove("./" + file_name + ".pkl")
+    assert num_inputs == ann_test.num_inputs
+    assert np.all(num_hidd == ann_test.num_hidden)
+    assert num_outputs == ann_test.num_outputs
+    for i in range(len(biases)):
+        assert np.all(biases[i] == ann_test.get_biases()[i])
+    for i in range(len(weights)):
+        assert np.all(weights[i] == ann_test.get_weights()[i])
+    assert activation_function == ann_test.get_activation_function()
+    assert loss_function == ann_test.get_loss_function()
+    
+    
+
+##########################################################################################################################
+
+#Test load and set the network method
+
+@given(inp = st.integers(min_value=1, max_value=1e5),
+       hidd = st.lists(st.sampled_from(num_neuron), min_size=1, max_size=10),
+       out = st.integers(min_value=1, max_value=50),
+       file_name = st.text(alphabet = st.characters(whitelist_categories = ('L', 'N'))))
+def test_bilding_network_from_saving_parameters(inp, hidd, out, file_name):
+    "Test that the neural network is loaded correctly from the saving file"
+    ann_test = ann.Ann(inp, hidd, out)
+    ann_test.set_activation_function(act.sigmoid)
+    ann_test.set_loss_function(lf.binary_cross_entropy)
+    ann_test.save_parameters(file_name)
+    ann_loaded = ann.Ann.load_and_set_network("./" + file_name + ".pkl")
+    os.remove("./" + file_name + ".pkl")
+    assert ann_loaded.num_inputs == ann_test.num_inputs
+    assert np.all(ann_loaded.num_hidden == ann_test.num_hidden)
+    assert ann_loaded.num_outputs == ann_test.num_outputs
+    for i in range(len(ann_test.get_biases())):
+        assert np.all(ann_loaded.get_biases()[i] == ann_test.get_biases()[i])
+    for i in range(len(ann_test.get_weights())):
+        assert np.all(ann_loaded.get_weights()[i] == ann_test.get_weights()[i])
+    assert ann_loaded.get_activation_function() == ann_test.get_activation_function()
+    assert ann_loaded.get_loss_function() == ann_test.get_loss_function()
 
 ############################################################################################################################
 
 #Test the activation functions
-
+"""
 @given(data())
 def test_range_sigmoid(data):
     "Test that the output of the sigmoid function is in the range [0, 1]"
@@ -281,6 +356,27 @@ def test_range_softmax_function_derivative(data):
 target_values = [0, 1]
 prediction_values = np.arange(0, 1 + 0.001, 0.001)
 
+
+@given(target = st.integers(min_value=0, max_value=1),
+       prediction = st.floats(min_value=0, max_value=1))
+def test_range_binary_cross_entropy(target, prediction):
+    "Test that the result of the binary cross entropy is in the expected interval [0, -*log(clip_value)]"
+    minimum = 0
+    maximum = - np.log(lf.clip_value)
+    binary_cross_entropy = lf.binary_cross_entropy(prediction, target)
+    assert binary_cross_entropy >= minimum and binary_cross_entropy <= maximum
+
+
+@given(target = st.integers(min_value=0, max_value=1),
+       prediction = st.floats(min_value=0, max_value=1))
+def test_range_binary_cross_entropy_deriv(target, prediction):
+    "Test that the result of the binary cross entropy derivative is in the expected interval [-1/clip_value, 1/clip_value]"
+    minimum = -1/lf.clip_value
+    maximum = 1/lf.clip_value
+    binary_cross_entropy_deriv = lf.binary_cross_entropy_deriv(prediction, target)
+    assert binary_cross_entropy_deriv >= minimum and binary_cross_entropy_deriv <= maximum
+
+
 @given(data())
 def test_range_cross_entropy(data):
     "Test that the result of the cross entropy is in the expected interval [0, -dim*log(clip_value)]"
@@ -316,20 +412,5 @@ def test_inf_values_cross_entropy_deriv(data):
     result = lf.cross_entropy_deriv(prediction, target)
     assert np.all(result > -np.inf) and np.all(result < np.inf)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
 
