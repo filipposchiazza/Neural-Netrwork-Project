a3be2b09d13b5a215d342dbb5c4b0b928063dae9
Filippo <filipposchiazza97@gmail.com>
2022-04-27 12:28:15 +0200

    Improve the documentation of the activation functions

--- neuralnet/activation_functions.py
+++ neuralnet/activation_functions.py
@@ -5,13 +5,38 @@ import numpy as np
 ##############################################################################
 
 def sigmoid(x):
-    "Definition of the sigmoid function"
+    """Definition of the sigmoid function
+    
+    Parameters
+    ----------
+    x : float
+        Input of the sigmoid function.
+
+    Returns
+    -------
+    TYPE : float
+        Output of the sigmoid function.
+
+    """
     x = np.where(x > -709, x, -709)    # Avoid problem of inf value in np.exp(x)
     return 1 / (1 + np.exp(-x))
 
 
 def deriv_sigmoid(x):
-    "Definition of the jacobian (trivial) of the sigmoid function"
+    """Definition of the jacobian (trivial) of the sigmoid function
+    
+    Parameters
+    ----------
+    x : float or array-like
+        Input of the derivative/jacobian of the sigmoid function.
+
+    Returns
+    -------
+    jacobian : numpy array
+        Jacobian matrix of the sigmoid function, evaluated for the input x; it is trivial because all the elements outside the principal diagonal are equal to zero.
+        In the particular case of a float as input, the ouput will be a numpy mono-dimensional array with one element (the simple derivative).
+
+    """
     x = np.array(x)
     lenght = np.size(x)
     jacobian = np.zeros((lenght, lenght))
@@ -22,14 +47,38 @@ def deriv_sigmoid(x):
 ##############################################################################
 
 def softmax(x):
-    "Definition of the softmax activation function"
+    """Definition of the softmax activation function
+    
+    Parameters
+    ----------
+    x : array-like
+        Input of the softmax function.
+
+    Returns
+    -------
+    TYPE : numpy array
+        Output of the softmax function, same dimension of the input.
+
+    """
     num = np.exp(x - np.max(x))
     den = np.sum(num)
     return num / den
 
 
 def deriv_softmax(x):
-    "Definition of the softmax function jacobian"
+    """Definition of the softmax function jacobian
+
+    Parameters
+    ----------
+    x : array-like
+        Input of the softmax function jacobian.
+
+    Returns
+    -------
+    jacobian : numpy array
+        Jacobian matrix of the softmax function, evaluated for the input x.
+
+    """
     lenght = np.size(x)
     a = softmax(x)
     jacobian = np.zeros((lenght, lenght))
