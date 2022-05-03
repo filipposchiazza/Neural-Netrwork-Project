a191bfc3134cc17442f69f3642d75566699efc2a
filipposchiazza <filipposchiazza97@gmail.com>
2022-05-03 17:05:26 +0200

    Add unit testing for the loss functions

--- test/testing.py
+++ test/testing.py
@@ -304,7 +304,7 @@ def test_bilding_network_from_saving_parameters(inp, hidd, out, file_name):
         assert np.all(ann_loaded.get_weights()[i] == ann_test.get_weights()[i])
     assert ann_loaded.get_activation_function() == ann_test.get_activation_function()
     assert ann_loaded.get_loss_function() == ann_test.get_loss_function()
-"""
+
 ############################################################################################################################
 
 #Test the activation functions
@@ -413,13 +413,49 @@ def test_range_softmax_function_derivative(inputs):
     jacobian = act.deriv_softmax(inputs)
     assert np.all(jacobian >= -1) and np.all(jacobian <= 1)
     
-    
+"""   
 ##################################################################################################################################
-"""
+
 #Test the loss functions
 
 target_values = [0, 1]
 prediction_values = np.arange(0, 1 + 0.001, 0.001)
+    
+    # binary cross entropy
+
+def test_specifi_value_binary_cross_entropy():
+    "Test specific output value of the binary cross entropy, when target = 1"
+    target = 1.
+    prediction = 0.7
+    binary_ce = lf.binary_cross_entropy(prediction, target)
+    expected_output = -np.log(0.7)
+    assert np.abs(binary_ce - expected_output) < 1e-10
+    
+
+def test_specifi_value_binary_cross_entropy_bis():
+    "Test specific output value of the binary cross entropy, when target = 0"
+    target = 0.
+    prediction = 0.2
+    binary_ce = lf.binary_cross_entropy(prediction, target)
+    expected_output = -np.log(1 - 0.2)
+    assert np.abs(binary_ce - expected_output) < 1e-10
+    
+
+def test_limit_cases_binary_cross_entropy():
+    "Test limit cases when the prediction is equal to 0 or 1"
+    assert np.abs(lf.binary_cross_entropy(prediction=1., target=1.)) < 1e-10
+    assert np.abs(lf.binary_cross_entropy(prediction=1., target=0.) + np.log(lf.clip_value)) < 1e-3
+    assert np.abs(lf.binary_cross_entropy(prediction=0., target=1.) + np.log(lf.clip_value)) < 1e-3
+    assert np.abs(lf.binary_cross_entropy(prediction=0., target=0.)) < 1e-10
+
+
+@given(target = st.integers(min_value=0, max_value=1),
+       prediction = st.floats(min_value=0., max_value=1.))
+def test_symmetric_property_binary_cross_entropy(prediction, target):
+    "Test the symmetry property of the binary cross entropy: same result for (prediction, target) and (1-prediction, 1-target)"
+    result = lf.binary_cross_entropy(prediction, target)
+    reverse_result = lf.binary_cross_entropy(1-prediction,1- target)
+    assert np.abs(result - reverse_result) < 1e-3
 
 
 @given(target = st.integers(min_value=0, max_value=1),
@@ -432,6 +468,35 @@ def test_range_binary_cross_entropy(target, prediction):
     assert binary_cross_entropy >= minimum and binary_cross_entropy <= maximum
 
 
+    # binary cross entropy deriv
+    
+    
+def test_specifi_value_binary_cross_entropy_deriv():
+    "Test specific output value of the binary cross entropy derivative, when target = 1"
+    target = 1.
+    prediction = 0.7
+    binary_ced = lf.binary_cross_entropy_deriv(prediction, target)
+    expected_output = -1 / 0.7
+    assert np.abs(binary_ced - expected_output) < 1e-10
+    
+
+def test_specifi_value_binary_cross_entropy_deriv_bis():
+    "Test specific output value of the binary cross entropy, when target = 0"
+    target = 0.
+    prediction = 0.3
+    binary_ced = lf.binary_cross_entropy_deriv(prediction, target)
+    expected_output = 1 / 0.7
+    assert np.abs(binary_ced - expected_output) < 1e-10
+    
+    
+def test_limit_cases_binary_cross_entropy_deriv():
+    "Test limit cases when the prediction is equal to 0 or 1"
+    assert np.abs(lf.binary_cross_entropy_deriv(prediction=1., target=1.) + 1) < 1e-10
+    assert np.abs(lf.binary_cross_entropy_deriv(prediction=1., target=0.) - 1/(1 - (1-lf.clip_value))) < 1e-3
+    assert np.abs(lf.binary_cross_entropy_deriv(prediction=0., target=1.) + 1/lf.clip_value) < 1e-3
+    assert np.abs(lf.binary_cross_entropy_deriv(prediction=0., target=0.) - 1) < 1e-10
+    
+
 @given(target = st.integers(min_value=0, max_value=1),
        prediction = st.floats(min_value=0, max_value=1))
 def test_range_binary_cross_entropy_deriv(target, prediction):
@@ -440,9 +505,49 @@ def test_range_binary_cross_entropy_deriv(target, prediction):
     maximum = 1/lf.clip_value
     binary_cross_entropy_deriv = lf.binary_cross_entropy_deriv(prediction, target)
     assert binary_cross_entropy_deriv >= minimum and binary_cross_entropy_deriv <= maximum
+    
+  
+@given(target = st.integers(min_value=0, max_value=1),
+       prediction = st.floats(min_value=1e-5, max_value=1-1e-5))
+def test_antisymmetric_property_binary_cross_entropy_deriv(prediction, target):
+    """Test that the result of the binary cross entropy derivative is antisymmetric 
+    for the change (prediction, target) -> (1-prediction, 1-target)"""
+    result = lf.binary_cross_entropy_deriv(prediction, target)
+    reverse_result = lf.binary_cross_entropy_deriv(1 - prediction, 1 - target)
+    assert np.abs(result + reverse_result) < 1e-5
+    
+    
+    # cross entropy
+    
+
+def test_specifi_value_cross_entropy():
+    "Test specific output value of the cross entropy"
+    target = [1., 0., 0.]
+    prediction = [0.7, 0.2, 0.1]
+    ce = lf.cross_entropy(prediction, target)
+    expected_output = -np.log(0.7)
+    assert np.abs(ce - expected_output) < 1e-10
+    
+
+def test_limit_case_cross_entropy():
+    "Test limit case of cross entropy when target and prediction are equal"
+    target = [1., 0., 0.]
+    prediction = [1., 0., 0.]
+    ce = lf.cross_entropy(prediction, target)
+    assert np.abs(ce) < 1e-10
+    
+    
+def test_limit_case_cross_entropy_bis():
+    "Test limit case of cross entropy when target and prediction are different end prediction elements are eighter 0. or 1."
+    target = [1., 0., 0.]
+    prediction = [0., 1., 0.]
+    ce = lf.cross_entropy(prediction, target)
+    expected_ce = np.log(lf.clip_value)
+    assert np.abs(ce + expected_ce) < 1e-10
 
 
-@given(data())
+# Here use data() rather than st.list(st.float, min, max) because it is required to have two lists of the same dimension
+@given(data()) 
 def test_range_cross_entropy(data):
     "Test that the result of the cross entropy is in the expected interval [0, -dim*log(clip_value)]"
     dim = data.draw(st.integers(min_value=2, max_value=20))
@@ -452,7 +557,37 @@ def test_range_cross_entropy(data):
     maximum = - dim * np.log(lf.clip_value)
     cross_entropy = lf.cross_entropy(prediction, target)
     assert cross_entropy >= minimum and cross_entropy <= maximum
+    
+    
+    # cross entropy derivative
+    
+    
+def test_specifi_value_cross_entropy_deriv():
+    "Test specific output value of the cross entropy derivative"
+    target = [1., 0., 0., 0.]
+    prediction = [0.65, 0.25, 0.005, 0.005]
+    ced = lf.cross_entropy_deriv(prediction, target)
+    expected_output = np.asarray([-1 / 0.65, 0., 0., 0.])
+    assert np.all(np.isclose(ced, expected_output, rtol=0.1, atol=1e-5))
+    
 
+def test_limit_case_cross_entropy_deriv():
+    "Test limit case of cross entropy derivative when target and prediction are equal"
+    target = [1., 0., 0., 0.]
+    prediction = [1., 0., 0., 0.]
+    ced = lf.cross_entropy_deriv(prediction, target)
+    expected_output = np.asarray([-1, 0, 0, 0])
+    assert np.all(np.isclose(ced, expected_output, rtol=0.1, atol=1e-5))
+    
+    
+def test_limit_case_cross_entropy_deriv_bis():
+    "Test limit case of cross entropy when target and prediction are different end prediction elements are eighter 0. or 1."
+    target = [0., 1., 0.]
+    prediction = [0., 0., 1.]
+    ced = lf.cross_entropy_deriv(prediction, target)
+    expected_output = np.asarray([-0/lf.clip_value, -1/lf.clip_value, 0.])
+    assert np.all(np.isclose(ced, expected_output, rtol=0.1, atol=1e-5))
+    
 
 @given(data())
 def test_nan_values_cross_entropy_deriv(data):
@@ -477,4 +612,3 @@ def test_inf_values_cross_entropy_deriv(data):
     result = lf.cross_entropy_deriv(prediction, target)
     assert np.all(result > -np.inf) and np.all(result < np.inf)
 
-"""
